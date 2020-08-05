af6ad1b76dc1732ba0da54f955f0134f704faeaf
Aakash Shrivastava <aakash.shrivastava@booking.com>
2020-08-06 00:59:21 +0200

    adding subheader

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,8 @@ function App() {
         </a> */}
       </header>
       <body>
-        test
+      <span>Artisan Made | Eco-Friendly | Handloom | Sustainable | Lifestyle Products.<br>Based in SoCal &amp; AMS <br>Shipping worldwide!🌎<br>💌~ info.chirita@gmail.com</span>
+      <a class="yLUwa" href="https://l.instagram.com/?u=https%3A%2F%2Fm.facebook.com%2FCHIRITAbyAnuja.K%2F&amp;e=ATNiH9gBpwxCyrYnhjf85vYEjr1_bLkbX8_YB98G7SULq80nvpZXtL7ThcRY5ckQrS7NzNzxPIJmf3-ZtZi-V3uozx7nwpdTWk_Cjg&amp;s=1" page_id="profilePage" rel="me nofollow noopener noreferrer" target="_blank">m.facebook.com/CHIRITAbyAnuja.K</a>
       </body>
     </div>
   );
