5004235db779c0e578a90a2af060f0de3bff52a9
Aakash Shrivastava <aakash.shrivastava@booking.com>
2020-08-06 00:49:08 +0200

    Updates

---
+++ precache-manifest.9a5fcae8a6dfbae676475f7a19944245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d723fa67a662be1da17096a3e4e2c0",
+    "url": "/chirita/index.html"
+  },
+  {
+    "revision": "bbd86e7410bcc6408343",
+    "url": "/chirita/static/css/main.0520d7c9.chunk.css"
+  },
+  {
+    "revision": "075a4cc3bdcfc253fbeb",
+    "url": "/chirita/static/js/2.0bdfeac5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chirita/static/js/2.0bdfeac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd86e7410bcc6408343",
+    "url": "/chirita/static/js/main.c3bceb56.chunk.js"
+  },
+  {
+    "revision": "0e5888268ab75aebbafc",
+    "url": "/chirita/static/js/runtime-main.c8f6fb47.js"
+  }
+]);
